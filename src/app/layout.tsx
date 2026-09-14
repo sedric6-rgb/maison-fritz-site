cd8fcff3160a66c998f540f452fcfd9dbbca0cba
Maison Fritz Site <site@maisonfritz.com>
2026-09-14 07:43:01 +0000

    Remplace la police de titres par Vraiment Riches Display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,500;9..144,600;9..144,650&family=Work+Sans:wght@400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
       </head>
